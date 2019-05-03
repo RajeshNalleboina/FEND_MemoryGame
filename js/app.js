14bd8aee9b74e1d737bb95d973a902f37a401703
Rajesh2545 <rajeshnalleboina94@gmail.com>
2019-05-03 21:01:51 +0530

    modified js file adding match cards moves & timer

--- js/app.js
+++ js/app.js
@@ -1,42 +1,90 @@
+// Create a list that holds all of your cards
 
- // Create a list that holds all of your cards
+var cards = [...document.querySelectorAll(".card")];
+var clickedCards = [];
+// Adding an event listener to each card.
+cards.map(i => {
+  i.addEventListener('click', displayCards);
+});
 
- var cards = [...document.querySelectorAll(".card")];
- var clickedCards=[];
- // Adding an event listener to each card.
- cards.map(i => {
-   i.addEventListener('click', displayCards);
- });
+var timeStatus= true;
+// * Display the cards on the page
+function displayCards() {
+  if (timeStatus==true) {
+    timeStatus=false;
+    timerCreation();
+  }
+  this.classList.add('open', 'show', 'disabled');
+  clickedCards.push(this);
+  cardsMatched();
+}
+//  shuffle the list of cards using the provided "shuffle" method
+var deck = document.querySelector(".deck");
+var cardShuffle = shuffle(cards);
+for (i in cardShuffle) {
+  [].forEach.call(cards, function(items) {
+    deck.appendChild(items);
+  });
+}
 
- // * Display the cards on the page
- function displayCards() {
-   this.classList.add('open', 'show', 'disabled');
-   clickedCards.push(this);
- }
- //  shuffle the list of cards using the provided "shuffle" method
- var deck = document.querySelector(".deck");
-   var cardShuffle = shuffle(cards);
-   for (i in cardShuffle) {
-     [].forEach.call(cards, function (items) {
-       deck.appendChild(items);
-     });
-   }
+// Matching the cards
+function cardsMatched() {
+  moveCounter();
+  setTimeout(function() {
+    if (clickedCards.length == 2) {
+      if (clickedCards[0].children[0].classList.item(1) === clickedCards[1].children[0].classList.item(1)) {
+        clickedCards.map(i => {
+          i.classList.add('match');
+          i.classList.remove('open', 'show');
+        });
+        gameOver();
+      } else {
+        clickedCards.map(i => {
+          i.classList.remove('open', 'show', 'disabled');
+        });
+      }
+      clickedCards = [];
+    }
+  }, 180);
+}
 
+// moves
+var move=document.querySelector('.moves');
+function moveCounter() {
+  moves=move.innerHTML;
+  moves++;
+  move.innerHTML=moves;
+}
 
+// timer Creation
+var timer=document.querySelector('.timer');
+var sec=0,
+mins=0;
+function timerCreation() {
+  timerInterval = setInterval(function() {
+  sec++;
+  if (sec==60) {
+    mins++;
+    sec=0;
+  }
+  timer.innerHTML=mins + " : "+sec;
+},1000);
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+  var currentIndex = array.length,
+    temporaryValue, randomIndex;
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
 
-    return array;
+  return array;
 }
 
 
